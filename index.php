a7bafe8e7d6f0f6a9f6fce9e8bb624f3253b864c
aji <mancode700@gmail.com>
2025-01-28 20:22:53 +0700

    ADD: Dockerfile

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
 <?php
-// Redirect ke halaman login jika belum login
-session_start();
-if (!isset($_SESSION['username'])) {
-    header("Location: login.php");
-    exit();
-} else {
-    // Redirect berdasarkan role
-    if ($_SESSION['role'] == 'admin') {
-        header("Location: admin/dashboard.php");
-    } else {
-        header("Location: user/dashboard.php");
-    }
-    exit();
-}
+header("Location: login.php");
+exit;
+?>
